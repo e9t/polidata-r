bd87343a198966b3ed4abac20f76bb92c7281cc9
Lucy Park <me@lucypark.kr>
2014-06-20 14:55:25 -0700

    Add Sunlight API interface

---
+++ R/SunlightAPI.R
@@ -0,0 +1,25 @@
+#' R interface for Sunlight API
+#'
+#' See \url{(https://sunlightlabs.github.io/congress/)}
+#'
+#' @export
+
+SunlightAPI <- function(method, region="korea", key=getOption("polrSunlightKey")) {
+    # TODO: auto navigate pages
+    # TODO: get API key with `getOption`
+    apiSource   <- "sunlight"
+    apiInfo     <- eval(parse(text=sprintf("api$%s", apiSource)))
+    #apiVersion  <- NULL
+
+    .MethodInAPI(apiSource, method)
+
+    baseurl <- paste(apiInfo$url, method, sep="/")
+    # TODO: get multiple args
+    args <- sprintf("apikey=%s", key)
+    url <- paste(baseurl, args, sep="?")
+    print(url)
+    # TODO: convert request elements' encoding
+    response <- jsonlite::fromJSON(url)
+
+    return(response$items)
+}
