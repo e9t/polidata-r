963c1a776e90453a80ab25a64f6db057baf9bc6c
Lucy Park <me@lucypark.kr>
2014-07-09 16:29:16 +0900

    Add search and summary toggle for POPONG API

--- R/PopongAPI.R
+++ R/PopongAPI.R
@@ -4,28 +4,37 @@
 #'
 #' @import jsonlite RCurl
 #' @export
-#' @param method method to call
+#' @param method Method to call
+#' @param search Search term for names
+#' @param getSummary Toggle to get summary data
 #' @param key API key
 #' @param debug Enable debugging mode
 #' @keywords API
 #' @seealso \code{\link{GoogleAPI}}, \code{\link{SunlightAPI}}
 #'
 
-PopongAPI <- function(method, key=getOption("polrPopongKey"), debug=FALSE) {
+PopongAPI <- function(method, search=NULL, getSummary=FALSE, key=getOption("polrPopongKey"), debug=FALSE) {
     # TODO: auto navigate pages
-    # TODO: get API key with `getOption`
     apiSource   <- "popong"
     apiAttrs    <- eval(parse(text=sprintf("apiInfo$%s", apiSource)))
     apiVersion  <- paste0("v", apiAttrs$version)
 
     MethodInAPI(apiSource, method)
 
-    paths <- c(apiAttrs$url, apiVersion, method, "")
-    query  <- list("api_key"=key)
+    if(is.null(search)) {
+        paths <- c(apiAttrs$url, apiVersion, method, "")
+        query <- list("api_key"=key)
+    } else {
+        paths <- c(apiAttrs$url, apiVersion, method, "search")
+        query <- list("api_key"=key, "q"=search)
+    }
+
     url   <- FormatURL(paths, query)
     jsontext <- RCurl::getURL(url)
     response <- jsonlite::fromJSON(jsontext)
+
     if(debug) { print(url) }
+    if(!(getSummary)) { response$items$summary <- NULL }
 
     return(response$items)
 }
