08dd438955e987ca57ab09e7da0102b4bd53774c
Lucy Park <me@lucypark.kr>
2014-10-05 05:10:37 +0000

    Update for data search

--- R/PopongAPI.R
+++ R/PopongAPI.R
@@ -13,7 +13,7 @@
 #' @seealso \code{\link{GoogleAPI}}, \code{\link{SunlightAPI}}
 #'
 
-PopongAPI <- function(method, search=NULL, getSummary=FALSE, key=getOption("PopongAPIKey"), debug=FALSE) {
+PopongAPI <- function(method, search=NULL, pages=1, pageSize=20, getSummary=FALSE, key=getOption("PopongAPIKey"), debug=FALSE) {
     apiSource   <- "popong"
     apiAttrs    <- eval(parse(text=sprintf("apiInfo$%s", apiSource)))
     apiVersion  <- paste0("v", apiAttrs$version)
@@ -28,12 +28,23 @@ PopongAPI <- function(method, search=NULL, getSummary=FALSE, key=getOption("Popo
         query <- list("api_key"=key, "q"=search)
     }
 
-    url      <- FormatURL(paths, query)
-    jsontext <- RCurl::getURL(url)
-    response <- jsonlite::fromJSON(jsontext)
+    itemlist <- list()
+    if(length(pages)>10) {
+        query <- c(query, "per_page"=200)
+        npages <- tail(pages, 1) - pages[1] + 1
+        pages <- ceiling(npages/10)
+    }
+    for(page in pages) {
+        cat(sprintf("Got page %s (n=%s)\n", page, pageSize))
+        query    <- c(query, "page"=page, "per_page"=pageSize)
+        url      <- FormatURL(paths, query)
+        jsontext <- RCurl::getURL(url)
+        response <- jsonlite::fromJSON(jsontext)
+        if(debug) { print(url) }
+        if(!(getSummary)) { response$items$summary <- NULL }
+        itemlist[[page]] <- response$items
+    }
 
-    if(debug) { print(url) }
-    if(!(getSummary)) { response$items$summary <- NULL }
-
-    return(response$items)
+    items = do.call(rbind, itemlist)
+    return(items)
 }
